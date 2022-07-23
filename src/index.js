c3da0769bd65b9a319b8cfed9d547ddcafb51c8f
NicoToff <toffolo01@hotmail.com>
2022-07-23 12:37:19 +0200

    Moved the JS to a separate file, fetched from repo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-const btnBeep = document.getElementById("beeper");
+const btnAlarm = document.getElementById("alarm");
 const btnStop = document.getElementById("stopper");
-btnBeep.addEventListener("click", e => {
+const btnSubmitPw = document.getElementById("submit-pw");
+const passwordField = document.getElementById("password-input");
+btnAlarm.addEventListener("click", e => {
     e.preventDefault();
-    $.post("/beep");
+    $.post("/alarm");
 });
 btnStop.addEventListener("click", e => {
     e.preventDefault();
     $.post("/stop");
 });
+btnSubmitPw.addEventListener("click", e => {
+    e.preventDefault();
+    console.log(passwordField.value);
+    const validator = new RegExp("^[A-Za-z0-9]{1,20}$");
+    const pw = passwordField?.value;
+    if (validator.test(pw)) {
+        $.ajax({
+            type: "post",
+            url: "/pw",
+            data: pw,
+            dataType: "text",
+            success: function (response) {
+                if (response === "OK") {
+                    btnAlarm.removeAttribute("disabled");
+                    localStorage.setItem("pw", "valid");
+                }
+            },
+        });
+    }
+    passwordField.value = "";
+});
+if (localStorage.getItem("pw") === "valid") {
+    btnAlarm.removeAttribute("disabled");
+    btnStop.removeAttribute("disabled");
+}
